68fca3a5b89517475ce70cbd463a9631826667e6
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-10-24 22:19:18 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,3 @@
-// routes/user.js
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
@@ -9,22 +8,22 @@ router.get('/', (req, res, next) => {
     if (!username) {
       return res.status(400).json({ error: 'Username query parameter "q" is required' });
     }
-    
     const now = Date.now();
     if (!data.users[username]) {
       data.users[username] = {
         online: true,
         lastPing: now,
-        otherStats: {} // Add any other user variables/stats here
+        otherStats: {},
+        notification: null
       };
     } else {
       data.users[username].online = true;
       data.users[username].lastPing = now;
     }
-    
-    res.json({ message: `User ${username} is now online`, user: data.users[username] });
+    const notification = data.notifications[username] || null;
+    res.json({ message: `User ${username} is now online`, user: data.users[username], notification });
   } catch (err) {
-    next(err); // Pass to error handler
+    next(err);
   }
 });
 
