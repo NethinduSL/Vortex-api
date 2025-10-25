16079223b7c2a49f88cfe573702afe3d2f5849ed
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-10-25 22:16:41 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,48 @@
-module.exports = {
+// In-memory data store
+const data = {
   users: {},
-  notifications: {},
-  games: {}
+  games: {},
+  notifications: {}
+};
+
+// Helper functions
+function updateUserOnlineStatus(username) {
+  if (data.users[username]) {
+    data.users[username].lastPing = Date.now();
+    data.users[username].online = true;
+  }
+}
+
+function cleanupInactiveUsers() {
+  const now = Date.now();
+  Object.keys(data.users).forEach(username => {
+    if (now - data.users[username].lastPing > 30000) { // 30 seconds
+      data.users[username].online = false;
+    }
+  });
+}
+
+function getOnlineUsers() {
+  cleanupInactiveUsers();
+  return Object.keys(data.users).filter(username => data.users[username].online);
+}
+
+function evaluateRPS(choice1, choice2) {
+  if (choice1 === choice2) return null; // tie
+  
+  const rules = {
+    'rock': 'officer',
+    'paper': 'rock', 
+    'officer': 'paper'
+  };
+  
+  return rules[choice1] === choice2 ? 'player1' : 'player2';
+}
+
+module.exports = {
+  data,
+  updateUserOnlineStatus,
+  cleanupInactiveUsers,
+  getOnlineUsers,
+  evaluateRPS
 };
