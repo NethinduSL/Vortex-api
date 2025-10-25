c5ff09db757a2e483a5c87270285a88666ba81bd
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-10-25 22:17:23 +0530

    Update online.js

--- routes/online.js
+++ routes/online.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data');
+const { getOnlineUsers } = require('../data');
 
-router.get('/', (req, res, next) => {
-  try {
-    const onlineUsers = Object.keys(data.users).filter(username => data.users[username].online);
-    res.json({ onlineUsers });
-  } catch (err) {
-    next(err);
-  }
+router.get('/', (req, res) => {
+  const onlineUsers = getOnlineUsers();
+  res.json(onlineUsers);
 });
 
 module.exports = router;
